a3a40f65f3dea11304a0d414c659ccea00890495
andaemin <eoals0216@gmail.com>
2025-10-25 15:15:40 +0900

    1day pratice ing..

--- 1day/variable.playground/Contents.swift
+++ 1day/variable.playground/Contents.swift
@@ -5,7 +5,9 @@ let age: Int = 25
 var height: Double = 173.1
 var isDeveloper = true
 
-print("이름 \(name)")
-print("나이")
+print("이름 : \(name)")
+print("나이 : \(age)")
+print("키 : \(height) cm")
+print("개발자 : \(isDeveloper)...?")
 
 
